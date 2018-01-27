9e991eee4d068fc27483f5158fc77f0bcbd1786d
Matt Nutsch <mattnutsch@gmail.com>
2018-01-27 09:17:58 -0600

    Add files via upload
    Added function wrappers.

--- readExchangeRatesGDAX.py
+++ readExchangeRatesGDAX.py
@@ -1,8 +1,14 @@
 # Name: readExchangeRatesGDAX.py
 # Author: Matt Nutsch
 # Date Created: 1-19-2018
-# Last Edited: 1-19-2018
+# Last Edited: 1-27-2018
 # Description: This script reads cryptocurrency exchange rate information from the exchange GDAX.
+
+#examples of calling the functions
+#print "The BCH-USD price is: " + getBCHToUSDFromGDAX();
+#print "The LTC-USD price is: " + getLTCToUSDFromGDAX();
+#print "The BTC-USD price is: " + getBTCToUSDFromGDAX();
+#print "The ETH-USD price is: " +  getETHToUSDFromGDAX();
 
 #import libraries
 import urllib, json, time
@@ -16,92 +22,115 @@ print "Reading the exchange rates from the GDAX exchange."
 
 ###################################################
 
-#BCH-USD
-#Get the BCH-USD price.
+def getBCHToUSDFromGDAX():
 
-#set up the URL
-url = "https://api.gdax.com/products/BCH-USD/ticker"
+	#BCH-USD
+	#Get the BCH-USD price.
 
-#read the page contents
-response = urllib.urlopen(url)
-data = json.loads(response.read())
+	#set up the URL
+	url = "https://api.gdax.com/products/BCH-USD/ticker"
 
-gdax_bchUsdPrice = data["price"]
+	#read the page contents
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
 
-#output the data so that we can see it
-#print "The raw output is: "
-#print data
+	gdax_bchUsdPrice = data["price"]
 
-print "The BCH-USD price is: " + gdax_bchUsdPrice
+	#output the data so that we can see it
+	#print "The raw output is: "
+	#print data
 
-#pause briefly
-time.sleep(.300)
+	#print "The BCH-USD price is: " + gdax_bchUsdPrice
 
-###################################################
-
-#ETH-USD
-#Get the ETH-USD price.
-
-#set up the URL
-url = "https://api.gdax.com/products/ETH-USD/ticker"
-
-#read the page contents
-response = urllib.urlopen(url)
-data = json.loads(response.read())
-
-gdax_ethUsdPrice = data["price"]
-
-#output the data so that we can see it
-#print "The raw output is: "
-#print data
-
-print "The ETH-USD price is: " + gdax_ethUsdPrice
-
-#pause briefly
-time.sleep(.300)
+	#pause briefly
+	time.sleep(.300)
+	
+	return gdax_bchUsdPrice;
 
 ###################################################
 
-#LTC-USD
-#Get the LTC-USD price.
+def getETHToUSDFromGDAX():
 
-#set up the URL
-url = "https://api.gdax.com/products/LTC-USD/ticker"
+	#ETH-USD
+	#Get the ETH-USD price.
 
-#read the page contents
-response = urllib.urlopen(url)
-data = json.loads(response.read())
+	#set up the URL
+	url = "https://api.gdax.com/products/ETH-USD/ticker"
 
-gdax_ltcUsdPrice = data["price"]
+	#read the page contents
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
 
-#output the data so that we can see it
-#print "The raw output is: "
-#print data
+	gdax_ethUsdPrice = data["price"]
 
-print "The LTC-USD price is: " + gdax_ltcUsdPrice
+	#output the data so that we can see it
+	#print "The raw output is: "
+	#print data
 
-#pause briefly
-time.sleep(.300)
+	#print "The ETH-USD price is: " + gdax_ethUsdPrice
+
+	#pause briefly
+	time.sleep(.300)
+	
+	return gdax_ethUsdPrice;
 
 ###################################################
 
-#BTC-USD
-#Get the BTC-USD price.
+def getLTCToUSDFromGDAX():
 
-#set up the URL
-url = "https://api.gdax.com/products/BTC-USD/ticker"
+	#LTC-USD
+	#Get the LTC-USD price.
 
-#read the page contents
-response = urllib.urlopen(url)
-data = json.loads(response.read())
+	#set up the URL
+	url = "https://api.gdax.com/products/LTC-USD/ticker"
 
-gdax_btcUsdPrice = data["price"]
+	#read the page contents
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
 
-#output the data so that we can see it
-#print "The raw output is: "
-#print data
+	gdax_ltcUsdPrice = data["price"]
 
-print "The BTC-USD price is: " + gdax_btcUsdPrice
+	#output the data so that we can see it
+	#print "The raw output is: "
+	#print data
 
-#pause briefly
-time.sleep(.300)
+	#print "The LTC-USD price is: " + gdax_ltcUsdPrice
+
+	#pause briefly
+	time.sleep(.300)
+	
+	return gdax_ltcUsdPrice;
+
+###################################################
+
+def getBTCToUSDFromGDAX():
+
+	#BTC-USD
+	#Get the BTC-USD price.
+
+	#set up the URL
+	url = "https://api.gdax.com/products/BTC-USD/ticker"
+
+	#read the page contents
+	response = urllib.urlopen(url)
+	data = json.loads(response.read())
+
+	gdax_btcUsdPrice = data["price"]
+
+	#output the data so that we can see it
+	#print "The raw output is: "
+	#print data
+
+	#print "The BTC-USD price is: " + gdax_btcUsdPrice
+
+	#pause briefly
+	time.sleep(.300)
+
+	return gdax_btcUsdPrice;
+
+###################################################
+	
+
+
+
+

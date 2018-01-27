db83fbd8a84ad849745a56399c07296fa414d09e
Matt Nutsch <mattnutsch@gmail.com>
2018-01-27 09:32:43 -0600

    Add files via upload

--- sendSMS.py
+++ sendSMS.py
@@ -1,28 +1,33 @@
 # Name: sendSMS.py
 # Author: Matt Nutsch
 # Date Created: 1-26-2018
-# Last Edited: 1-26-2018
+# Last Edited: 1-27-2018
 # Description: This script sends a SMS message through the service Tropo.
+
+#example usage
+print "Sending SMS message."
+tropoNumberToDial = "15551112222"
+tropoMsg = "LoremIpsum"
+sendSMSMessage(tropoNumberToDial, tropoMsg);
 
 import urllib, json, time
 
 #DEV NOTE: Replace these with values read from the configuration.py file.
 tropoAPIToken = "11111111111111111111111111111111111111111111111"
 tropoSMSAuth = "22222222222222"
-tropoNumberToDial = "15551112222"
-tropoMsg = "LoremIpsum"
 
-#DEV NOTE: url encode message
-#tropoMsg = urllib.parse.quote_plus(tropoMsg)
+def sendSMSMessage(phoneNumberToText, messageToSend):
 
-url = "https://api.tropo.com/1.0/sessions?action=create&token=" + tropoAPIToken + "&numberToDial=" + tropoNumberToDial + "&msg=" + tropoMsg
+	#DEV NOTE: url encode message
+	#messageToSend = urllib.parse.quote_plus(messageToSend)
 
-print "Sending SMS message."
+	url = "https://api.tropo.com/1.0/sessions?action=create&token=" + tropoAPIToken + "&numberToDial=" + phoneNumberToText + "&msg=" + messageToSend
 
-#read the page contents
-response = urllib.urlopen(url)
+	#read the page contents
+	response = urllib.urlopen(url)
 
-#DEV NOTE: replace this with error detection
-print "The Tropo response is: " + str(response)
+	#DEV NOTE: replace this with error detection
+	#print "The Tropo response is: " + str(response)
 
+	return;
 

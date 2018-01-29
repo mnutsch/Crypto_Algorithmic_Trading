310d1058662e2f6055c6658a36d1e8a1a754c693
Timothy Bramlett <timothy.bramlett@gmail.com>
2018-01-28 21:05:18 -0800

    adding more code for basic functionality to main.py

--- main.py
+++ main.py
@@ -1,10 +1,52 @@
 #!/usr/bin/python
+import time
+import readExchangeRatesGDAX
+import readExchangeRatesGemini
 
+INTERVAL = 60 # seconds
+
+
+
+def get_all_rates():
+    """TODO: If we decide we need it"""
+    pass
 
 
 def main():
 
-    print 'running the app.'
+
+    while True:
+
+        # Get the rates from all the exchanges
+        print '\n\nCalling readExchangeRatesGemini()'
+        print 'Calling readExchangeRatesGDAX'
+
+        BTCToUSDFromGDAX = float(readExchangeRatesGDAX.getBTCToUSDFromGDAX())
+        BTCToUSDFromGemini = float(readExchangeRatesGemini.getBTCToUSDFromGemini())
+
+        
+        print ''
+        print 'BTCToUSDFromGDAX: {}'.format(BTCToUSDFromGDAX) 
+        print 'BTCToUSDFromGemini: {}'.format(BTCToUSDFromGemini)
+        
+
+        # Analyze the rates to check for opportunities
+        print '\nAnalyzing the rates...'
+        diff = BTCToUSDFromGDAX - BTCToUSDFromGemini
+        print 'Diff: {}'.format(diff)
+
+        # now lets check the abs diff between the two
+        if abs(diff) >= 50.0:
+            print '\t!!!! HELLO OPPORTUNITY WORLD!!!!!'
+
+
+        # Notify if opportunities exist
+        print '\nIf opportunities exist...'
+        print 'I will call sendSMS()'
+
+        # Wait for some specified interval
+        # Repeat step one
+        time.sleep(INTERVAL)
 
 
 

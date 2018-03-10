36fc468354186cad53e23c69a67936819eb084fe
Patrick <mullanep@oregonstate.edu>
2018-03-10 22:33:49 +0000

    Refactored from other files

---
+++ exchange.py
@@ -0,0 +1,69 @@
+# Name: exchange.py
+# Author: Patrick Mullaney
+# Date Created: 3-1-2018
+# Last Edited: 3-10-2018
+# Description: This file provides functions for exchange objects.
+
+import currency
+
+# Review deposit fees...add getVolume?
+
+# Exchange object contains information relevant to a currency at that exchange.    
+class Exchange(): 
+    name = None
+    currency = None
+    price = None
+    depositFee = None
+    withdrawFee = None
+    exchangeFee = None
+    volume = 0.00
+
+################################################################################
+
+# Returns currency info at a given exchange.
+def getExchange(xchg, curr, vol):
+    exchgInfo = Exchange()
+    exchgInfo.name = xchg
+    exchgInfo.currency = curr
+    exchgInfo.price = currency.getPrice(xchg, curr)
+    exchgInfo.depositFee = currency.getDepositFee(xchg, curr)
+    exchgInfo.withdrawFee = currency.getWithdrawFee(xchg, curr)
+    exchgInfo.exchangeFee = currency.getExchangeFee(xchg, curr)
+    return exchgInfo
+    
+################################################################################
+
+# Returns currency info at a given exchange.
+def getExchange1(xchg, curr):
+    exchgInfo = Exchange()
+    exchgInfo.name = xchg
+    exchgInfo.currency = curr
+    exchgInfo.price = currency.getPrice(xchg, curr)
+    exchgInfo.depositFee = getDepositFee(xchg, curr)
+    exchgInfo.withdrawFee = currency.getWithdrawFee(xchg, curr)
+    exchgInfo.exchangeFee = currency.getExchangeFee(xchg, curr)
+    return exchgInfo
+
+################################################################################
+
+# Review get deposit fee - should there be a fee or is there a volume %?
+# Returns deposit % fee for a currency at a given exchange.
+def getDepositFee(xchg, curr):
+    fee = 0
+    if xchg == 'gdax':
+        if curr == 'BCH':
+            fee = 0.00
+        elif curr == 'ETH':
+            fee = 0.00
+        elif curr == 'LTC':
+            fee = 0.00
+        elif curr == 'BTC':
+            fee = 0.00
+    elif xchg == 'gemini':
+        if curr == 'BTC':
+            fee = 0.00
+        elif curr == 'ETH':
+            fee = 0.00
+    return fee
+
+################################################################################

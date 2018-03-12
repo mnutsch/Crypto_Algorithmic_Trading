b526f5f90b45c0d701bdb6cbeca8c178324cc295
EC2 Default User <ec2-user@ip-10-0-77-228.us-west-2.compute.internal>
2018-03-12 02:51:25 +0000

    working on main.py

--- main.py
+++ main.py
@@ -2,28 +2,24 @@
 import time
 import readExchangeRatesGDAX
 import readExchangeRatesGemini
-import configuration
+from configuration import *
 import sendSMS
 import urllib
 import sendEmail
 import os
 import checkForSingleCurrencyOpps
-import inspect
-from pprint import pprint
+# import inspect
+# from pprint import pprint
 
-INTERVAL = 60 # seconds
-# PHONE_NUMBERS = ['14696488502', '12539612914']
-PHONE_NUMBERS = ['14696488502']
-THRESHHOLD = 0
-EMAIL_LIST = ['timothy.bramlett@gmail.com', 'timbram@gmail.com']
 
 # just for testing
-sendEmail.sendSingleEmail('timothy.bramlett@gmail.com', 'main.py starting up...')
+# sendEmail.sendSingleEmail('timothy.bramlett@gmail.com', 'main.py starting up...')
 
 
-def notify_subscribed_via_email(message):
+def notify_subscribed_via_email(opp, message):
     for email in EMAIL_LIST:
         sendEmail.sendSingleEmail(email, message)
+        # sendEmail.sendSingleEmailTemplate(opp)
 
 def notify_subscribed_via_sms(message):
     for phone in PHONE_NUMBERS:
@@ -31,7 +27,6 @@ def notify_subscribed_via_sms(message):
 
 
 def main():
-
 
     while True:
 
@@ -42,7 +37,7 @@ def main():
                 message = """New Arbitrage opportunity: {}
                 """.format(vars(opportunity))
                 notify_subscribed_via_sms(message)
-                notify_subscribed_via_email(message)
+                notify_subscribed_via_email(opportunity, message)
 
         # Wait for some specified interval
         # Repeat step one

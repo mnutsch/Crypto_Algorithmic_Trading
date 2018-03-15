d45aa9f35471d155ddb7e5ca0c5264ae0a03fdec
Patrick <mullanep@oregonstate.edu>
2018-03-15 22:25:31 +0000

    added functionality

--- checkForMultipleCurrencyOpps.py
+++ checkForMultipleCurrencyOpps.py
@@ -6,10 +6,11 @@
 # Description: This script checks for multiple currency arbitrage opportunities.
 
 import readExchangeRatesGDAX, readExchangeRatesGemini
-# Used in permutations.
-import itertools
+# Used in permutations and calculations.
+import itertools, numpy
 # Files used for calculations, currency info, and forming chain of transactions.
 import calc, currency, conversions, chains
+from configuration import OUTPUT
 
 DEBUG = False
 
@@ -46,28 +47,23 @@ def printCost(curr1, curr2):
 ################################################################################
 # Returns revenue for a transaction.
 def calcMultRev(curr1, curr2):
+    # Calculate revenue.
     revenue = 0.00
-    #printTrans(curr1)
-    #printTrans(curr2)
+    # Get deposit cost.
     depositCost = currency.getFeeCost(curr2, curr2.depositFee)
-    #print "51, dep cost: ", depositCost
     revenue = ((curr2.amount * curr2.price) * depositCost)/curr2.price
+    # Get 1st currency exchange fee.
     curr1XchgFee = currency.getFeeCost(curr1, curr1.exchangeFee)
-    #print "56, xchg1: ", curr1XchgFee
     revenue = revenue * curr1XchgFee 
-    #print "58 rev: ", revenue
-    #revenue = revenue * (curr1.price)
     revenue = revenue * (curr1.price * curr1.amount)
-    #print "60 rev: ", revenue
+    # Get 2nd currency exchange fee.
     curr2XchgFee = currency.getFeeCost(curr2, curr2.exchangeFee)
     revenue = revenue * curr2XchgFee
-    #print "62 rev: ", revenue
+    # Factor in withdrawal costs.
     withdrawCost = currency.getFeeCost(curr1, curr1.withdrawFee)
     revenue = revenue * withdrawCost
     return revenue
-    # rev = ((((B13             *B4)            *(100-B5)/100)/B4      *(100-B7)/100)     *C4          *(100-C7)/100)*(100-C10)/100
-    #revenue = ((curr2.amount * curr2.price) * depositCost)/curr2.price * curr1XchgFee * curr1.price * curr2XchgFee * withdrawCost
-    
+  
 ################################################################################
 
 # Returns profit loss for a currency transaction for multiple arbitrage (potentially
@@ -78,49 +74,39 @@ def multProfitLoss(curr1, curr2, amount):
     # Amount of coins to sell and value of sale.
     curr1.amount = amount/curr1.price
     sellValue = curr1.amount * curr1.price
-    #print "amountToSell: {} (${})".format(str(amountToSell),str(sellValue))
     # Amount of coins to buy and value.
     curr2.amount = sellValue/curr2.price
     buyValue = curr2.amount * curr2.price
-    #print "amountToBuy: {} (${})".format(str(amountToSell), str(sellValue))
     revenue = buyValue - sellValue
-    
-    #printTrans(curr1)
-    #printTrans(curr2)
     #revenue = ((curr2.amount * curr2.price) * depositCost)/curr2.price * curr1XchgFee * curr1.price * curr2XchgFee * withdrawCost
     #print "OG revenue: ", revenue
     revenue = calcMultRev(curr1, curr2)
     # Profit/loss = revenue - investment.
-    investment = (curr1.price * curr1.amount)
-    #print "103 investment: ", investment
+    #investment = (curr1.price * curr1.amount)
+    #print "103 investment: ", investment # for debugging.
     profitLoss = revenue - (curr1.price * curr1.amount)
-    #profitLoss = revenue - (curr1.price * amount)
     return profitLoss
 
 ################################################################################
 
 # Returns the calculated profit/loss for 3 currencies in a transaction chain
-# (alternative method explored to attempt to produce better results).
+# (alternative method was explored to attempt to produce better results).
 def profitLoss3(curr1, curr2, curr3, amount):
     profitLoss = 0.00
     # Amount of coins to sell and value of sale.
     amountToSell = amount/curr1.price
     sellValue = amountToSell * curr1.price
-    #print "amountToSell: {} (${})".format(str(amountToSell),str(sellValue))
     # Amount of coins to buy and value of buy.
     curr2.amount = sellValue/curr2.price
     buyValue = curr2.amount * curr2.price
-    #print "amountToBuy: {} (${})".format(str(curr2.amount), str(buyValue))
     # Revenue from first transaction.
     revenue = calcMultRev(curr1, curr2)
     # Profit/loss = revenue - investment of first transaction.
     firstProfitLoss = revenue - (curr1.price * curr1.amount)
-    
     # Amount of coins to sell.
     amountToSell = amount/curr2.price
     # Value of sale
     sellValue = amountToSell * curr2.price
-    #print "2. amountToSell: {} (${})".format(str(amountToSell),str(sellValue))
     # Amount of coins to buy.
     curr3.amount = sellValue/curr3.price
     buyValue = curr3.amount * curr3.price
@@ -149,16 +135,12 @@ def checkOpps(amount):
     
     # Get list of profitable chains of transactions.
     profitable = getProfitableChains(transactionChains, amount)
-    
+    # If there are any opportunities, create list.
     opportunities = []
     if len(profitable) > 0:
-        #print "First profit: "
-        #chains.printChain(profitable[0])
-        #chains.chainProfitLoss(profitable[0])
         # Create opportunity object with relevant info for each profitable opportunity.
         for i in profitable:
             newOpp = multipleOpportunity()    
-            # Add currencies, starting amt
             newOpp.startingAmount = amount
             newOpp.profitLoss = chains.chainProfitLoss(i)
             newOpp.transactionChain = chains.chainMessage(i)
@@ -196,29 +178,101 @@ def checkOppsCost(amount):
     currencyList = currency.getCurrencyList()
     # Get all permutations of currency list.
     currencyPermutations = list(itertools.permutations(currencyList, 2))
-    # Get list of permutations of all currency cost comparisons for chain up to three transactions.
-    compPermutations = list(itertools.permutations(currencyPermutations, 3))
+    '''  Calculating by conversion costs was not as profitable.
     # Get list of conversion costs.
     conversionCosts = conversions.getConversionList(amount, currencyList)
-    #for i in conversionCosts:
-    #   print "{}->{}: {}".format(str(i.curr1.name), i.curr2.name, str(i.cost))
-    
     # Get list of positive conversion transactions.
     positiveConversions = conversions.getPositiveConversions(amount, currencyList)
-    #print "0: ", positiveConversions[0]
-    #print "Transaction: {}->{}: {}".format(str(positiveConversions[0][0].curr1.name), str(positiveConversions[0][0].curr2.name), str(positiveConversions[0][0].cost))
-    # print positive conv 0
-    #for i in positiveConversions[0]:
-    #    print "Transaction: {}({})->{}({}): {}".format(str(i.curr1.name), str(i.curr1.exchange), str(i.curr2.name), str(i.curr2.exchange), str(i.cost))
-    
+    '''
     # Get list of transaction chains up to 3 transactions.
     transactionChains = chains.getTransactionChains(currencyList)
     #print "transaction chain length: ", len(transactionChains)
     
     # Get list of profitable chains of transactions.
     profitable = getProfitableChains(transactionChains, amount)
-
-    print "First profit: "
-    chains.printChain(profitable[0])
-    chains.chainProfitLoss(profitable[0])
     return profitable
+
+################################################################################
+# Added new code below:
+
+# Checks for multiple arbitrage opportunities, taking into consideration
+# maximum cost and minimum profit.
+def checkAllbyProfit(maxCost, minProfit):
+    # Get list of currencies.
+    currencyList = currency.getCurrencyList()
+    # Get all permutations of currency list.
+    currencyPermutations = list(itertools.permutations(currencyList, 2))
+    possibleTransactions = len(currencyPermutations)
+    if OUTPUT:
+        print "There are {} possible transactions...".format(str(possibleTransactions))
+    # Get list of transaction chains up to 3 transactions.
+    transactionChains = chains.getTransactionChains(currencyList)
+    possibleChains = len(transactionChains)
+    if OUTPUT:
+        print "Checking {} possible transaction chains...".format(str(possibleChains))
+    
+    amount = maxCost
+    # Get list of profitable transaction chains..
+    profitable = getProfitableChainsByProfit(transactionChains, maxCost)
+    opportunities = []
+    # If there are any profitable transaction chains, return opportunities.
+    if len(profitable) > 0:
+        # Create opportunity object with relevant info for each profitable opportunity.
+        for i in profitable:
+            newOpp = multipleOpportunity()    
+            # Add currencies, starting amt
+            newOpp.startingAmount = amount
+            newOpp.profitLoss = chains.chainProfitLoss(i)
+            newOpp.transactionChain = chains.chainMessage(i)
+            opportunities.append(newOpp)
+    if OUTPUT is True and len(profitable) == 0:
+        print "Currently no profitable multiple currency opportunities."
+    # Return opportunities.
+    return opportunities  
+  
+################################################################################
+
+# Returns a list of profitable chain of transactions.
+def getProfitableChainsByProfit(transactionChains, amount):
+    # List of pofitable transaction chains.
+    profitable = []
+    # Iterate through chain of transactions.
+    for chain in transactionChains:
+        #chainProfit = 0
+        # Changed this from .001 to .1 for performance reasons.
+        oppList = numpy.arange(0.1, amount, 0.1)
+        for i in oppList:
+            chainProfit = 0
+            maxProfitLoss = -999999999.99
+            # Sum total profitLoss for chain.
+            for transaction in chain:
+                transaction.curr1.amount = (i/transaction.curr1.price)
+                transaction.profitLoss = multProfitLoss2(transaction.curr1,transaction.curr2, transaction.curr1.amount)
+                chainProfit += transaction.profitLoss
+            # Update profitLoss if appropriate.
+            if chainProfit > maxProfitLoss:
+                maxProfitLoss = chainProfit
+            # If profitLoss for chain of transactions is profitable, add to list.
+            if chainProfit > 0: 
+                profitable.append(chain)
+    return profitable
+
+################################################################################
+# Returns profit loss for a currency transaction for multiple arbitrage 
+#(potentially different exchanges, etc.).
+def multProfitLoss2(curr1, curr2, amount):
+    profitLoss = 0
+    # Amount of coins to sell and value of sale.
+    curr1.amount = amount/curr1.price
+    sellValue = curr1.amount * curr1.price
+    # Amount of coins to buy and value.
+    curr2.amount = sellValue/curr2.price
+    buyValue = curr2.amount * curr2.price
+    revenue = buyValue - sellValue
+    revenue = calcMultRev(curr1, curr2)
+    # Profit/loss = revenue - investment.
+    #investment = (curr1.price * curr1.amount) - used in debugging.
+    profitLoss = revenue - (curr1.price * curr1.amount)
+    return profitLoss
+
+################################################################################
